177131649f0073efbe0b0f400a9beea2024f7ada
Varun <varunpatel207@gmail.com>
2022-08-21 11:11:08 -0400

    - removed code for 238 - DIL

--- src/leetcode/238_product_of_array_except_self.py
+++ src/leetcode/238_product_of_array_except_self.py
@@ -1,16 +1,9 @@
+# TODO: Finish this
+
 class Solution:
     def productExceptSelf(self, nums):
         answer = []
 
-        total_product = 1
-        for index, num in enumerate(nums):
-            total_product = total_product * num
-
-        for j, num in enumerate(nums):
-            if num > 0:
-                answer.append(int(total_product / num))
-            else:
-                answer.append(int())
 
         return answer
 
