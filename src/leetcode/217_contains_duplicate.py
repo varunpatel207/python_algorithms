a7539af0869e04d3f2de8377cf4a1036908be47c
Varun <varunpatel207@gmail.com>
2022-09-20 12:37:15 -0400

    - 53_contains_duplicate

--- src/leetcode/217_contains_duplicate.py
+++ src/leetcode/217_contains_duplicate.py
@@ -1,15 +1,22 @@
 class Solution:
     def containsDuplicate(self, nums):
-        num_list = set()
+        nums.sort()
 
-        for num in nums:
-            if num in num_list:
+        for index, num in enumerate(nums[:-1]):
+            if num == nums[index + 1]:
                 return True
-            num_list.add(num)
-
         return False
 
-nums = [1,5,6,3,9,1]
+        # num_list = set()
+        #
+        # for num in nums:
+        #     if num in num_list:
+        #         return True
+        #     num_list.add(num)
+        #
+        # return False
+
+nums = [9,5,6,3,9]
 s = Solution()
 output = s.containsDuplicate(nums=nums)
 print(output)
